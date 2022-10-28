6c7da1f400ad6bb36c7a033fed484f304102c26c
Nazar <fedunnazar20122001@gmail.com>
2022-10-28 16:00:04 +0300

    deserialize works without photo read string

--- TCPUserMessage/UserMessage.cs
+++ TCPUserMessage/UserMessage.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Drawing;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Image = System.Drawing.Image;
 
 namespace TCPUserMessage
 {
@@ -16,17 +21,20 @@ namespace TCPUserMessage
         public string Id { get; set; }
         public string Name { get; set; }
         public string Text { get; set; }
+        public string Photo { get; set; }
 
         public byte[] Serialize()
         {
             using (MemoryStream ms = new MemoryStream())
             {
+                
                 using (BinaryWriter bw = new BinaryWriter(ms))
                 {
                     bw.Write((int)MessageType);
                     bw.Write(Id);
                     bw.Write(Name);
                     bw.Write(Text);
+                    bw.Write(Photo);
                 }
                 return ms.ToArray();
             }
@@ -43,6 +51,15 @@ namespace TCPUserMessage
                     userMsg.Id = br.ReadString();
                     userMsg.Name = br.ReadString();
                     userMsg.Text = br.ReadString();
+                    try
+                    {
+                        userMsg.Photo = br.ReadString(); // PROBLEM
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    
                 }
             }
             return userMsg;
